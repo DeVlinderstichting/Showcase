1a2f5e2e68d22be16c596631d9959ea1ec349440
Sil Nieuwhof <sil.nieuwhof@sn-it.nl>
2022-03-21 14:13:07 +0100

    start with landingpage

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title')
+Welcome to showcase
+@endsection
+
+@section('content')
+<div class="row title-container">
+    <div class="col d-flex">
+        <div class="sign-in-container">
+            <h1 class="text-center">Showcase</h1>
+            <h3 class="text-center">Een subtitle</h3>
+            <h3 class="text-center">Nog een subtitle</h3>
+        </div>
+    </div>
+    
+</div>
+                                    
+                                
+@endsection

af42e41be4eac60135210ba017dd774ac3aab3ae
Bas Oteman <bas.oteman@vlinderstichting.nl>
2022-03-21 15:25:07 +0100

    update to routes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ use DB;
 //this controller is in charge of creating a package with all the user information, required to set up the app (the first time). This package should contain the species/settings/eba's etc 
 class UserController extends Controller
 {
-    public function welcome()
+    public function showLogin()
     {
         return view ('userLogin');
     }

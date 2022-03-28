ee90ffde0efaa7a19ca2e32fcface0ca7465ec94
Sil Nieuwhof <sil.nieuwhof@sn-it.nl>
2022-03-28 14:52:55 +0200

    changed col-width

--- resources/views/visitCreate.blade.php
+++ resources/views/visitCreate.blade.php
@@ -46,7 +46,7 @@
         <div class="card mb-2">
             <div class="card-body">
                 <label for="date" class="col-md-3 col-form-label">Date</label>
-                <div class="col-md-9">
+                <div class="col">
                     <input type="date" class="form-control @if($errors->has('startdate')) is-invalid @endif" max={{$maxDate}} min={{$minDate}} id="startdate" name="startdate" value="{{old('startdate')}}"}}>
                     @if($errors->has('startdate')) 
                         <div class="invalid-feedback"> {{$errors->first('startdate')}} </div>
@@ -54,14 +54,14 @@
                 </div>
                 <label for="starttime" class="col-md-3 col-form-label">
                 @if($isSingle)Time @else Starttime @endif</label>
-                <div class="col-md-9">
+                <div class="col">
                     <input type="time" class="form-control @if($errors->has('starttime')) is-invalid @endif" id="starttime" name="starttime" value="{{old('starttime')}}">
                     @if($errors->has('starttime')) <div class="invalid-feedback"> {{$errors->first('starttime')}} </div>@endif
                 </div>
 
                 @if (!$isSingle)
                     <label for="endtime" class="col-md-3 col-form-label">Endtime</label>
-                    <div class="col-md-9">
+                    <div class="col">
                         <input type="time" class="form-control @if($errors->has('endtime')) is-invalid @endif" id="endtime" name="endtime" value="{{old('endtime')}}">
                         @if($errors->has('endtime')) <div class="invalid-feedback"> {{$errors->first('endtime')}} </div>@endif
                     </div>
@@ -73,7 +73,7 @@
                         flowerid
                     @endif
                 @endif
-                <div class="col-md-9">
+                <div class="col">
                     <label for="endtime" class="col-md-3 col-form-label">Notes</label>
                     <input type="text" class="form-control @if($errors->has('notes')) is-invalid @endif" id="notes" name="notes" value="{{old('notes')}}"}}>
                     @if($errors->has('notes')) 
@@ -82,17 +82,17 @@
                 </div>
                     @if(($isTransect) || ($isTimed))
                         <label for="cloud" class="col-md-3 col-form-label">Cloud cover</label>
-                        <div class="col-md-9">
+                        <div class="col">
                             <input type="time" class="form-control @if($errors->has('cloud')) is-invalid @endif" id="cloud" name="cloud" value="{{old('cloud')}}">
                             @if($errors->has('cloud')) <div class="invalid-feedback"> {{$errors->first('cloud')}} </div>@endif
                         </div>
                         <label for="wind" class="col-md-3 col-form-label">Wind speed</label>
-                        <div class="col-md-9">
+                        <div class="col">
                             <input type="time" class="form-control @if($errors->has('wind')) is-invalid @endif" id="wind" name="wind" value="{{old('wind')}}">
                             @if($errors->has('wind')) <div class="invalid-feedback"> {{$errors->first('wind')}} </div>@endif
                         </div>
                         <label for="temp" class="col-md-3 col-form-label">Temperature</label>
-                        <div class="col-md-9">
+                        <div class="col">
                             <input type="time" class="form-control @if($errors->has('temp')) is-invalid @endif" id="temp" name="temp" value="{{old('temp')}}">
                             @if($errors->has('temp')) <div class="invalid-feedback"> {{$errors->first('temp')}} </div>@endif
                         </div>
@@ -128,7 +128,7 @@
                     </table>
                 </div>
                 <b>Add species</b>
-                <div class="col-md-9 mb-3">
+                <div class="col mb-3">
                     <select class="add-species-select w-100"></select>
                 </div>
 

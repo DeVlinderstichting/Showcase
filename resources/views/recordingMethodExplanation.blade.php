8e9f236c04368860f7abe807554f443181355aef
Sil Nieuwhof <sil.nieuwhof@sn-it.nl>
2022-05-25 11:42:28 +0200

    changed monitoring and identification pages contents

--- resources/views/recordingMethodExplanation.blade.php
+++ resources/views/recordingMethodExplanation.blade.php
@@ -10,57 +10,57 @@ Guide to monitoring
     <h2 class="px-4">Subtitle</h2>
 </div>
 <div class="container text-center mt-3 mb-3">
-    <a href="#nl" class="btn btn-primary">Netherlands</a>
-    <a href="#gb" class="btn btn-primary">United Kingdom</a>
-    <a href="#es" class="btn btn-primary">Spain</a>
-    <a href="#de" class="btn btn-primary">Germany</a>
+    <a href="#special" class="btn btn-primary">I saw something special</a>
+    <a href="#15min" class="btn btn-primary">15 minutes count</a>
+    <a href="#transect" class="btn btn-primary">Walk transect</a>
+    <a href="#fit" class="btn btn-primary">Fit count</a>
 </div>
-<div class="container mb-3" id="nl">
+<div class="container mb-3" id="special">
     <div class="row">
         <div class="col-md d-flex p-4">
             <img src="images/bf6.jpg" class="img-fluid"> 
         </div>
         <div class="col-md d-flex flex-column p-4">
-            <h2>Netherlands</h2>
+            <h2>I saw something special</h2>
             <h4>Line 1</h4>
             <h4>Line 2</h4>
             <h4>Line 3</h4>
         </div>
     </div>
 </div>
-<div class="container mb-3" id="gb">
+<div class="container mb-3" id="15min">
     <div class="row switch-direction">
         <div class="col-md d-flex p-4">
             <img src="images/bf6.jpg" class="img-fluid"> 
         </div>
         <div class="col-md d-flex flex-column p-4">
-            <h2>United Kingdom</h2>
+            <h2>15 minutes count</h2>
             <h4>Line 1</h4>
             <h4>Line 2</h4>
             <h4>Line 3</h4>
         </div>
     </div>
 </div>
-<div class="container mb-3" id="es">
+<div class="container mb-3" id="transect">
     <div class="row">
         <div class="col-md d-flex p-4">
             <img src="images/bf6.jpg" class="img-fluid"> 
         </div>
         <div class="col-md d-flex flex-column p-4">
-            <h2>Spain</h2>
+            <h2>Walk transect</h2>
             <h4>Line 1</h4>
             <h4>Line 2</h4>
             <h4>Line 3</h4>
         </div>
     </div>
 </div>
-<div class="container mb-3" id="de">
+<div class="container mb-3" id="fit">
     <div class="row switch-direction">
         <div class="col-md d-flex p-4">
             <img src="images/bf6.jpg" class="img-fluid"> 
         </div>
         <div class="col-md d-flex flex-column p-4">
-            <h2>Germany</h2>
+            <h2>Fit count</h2>
             <h4>Line 1</h4>
             <h4>Line 2</h4>
             <h4>Line 3</h4>
--- resources/views/speciesIdentificationHelp.blade.php
+++ resources/views/speciesIdentificationHelp.blade.php
@@ -9,9 +9,163 @@ Species Identification
     <h1 class="p-4">Species Identification</h1>
     <h2 class="px-4">Subtitle</h2>
 </div>
-<div class="container mb-3">
+<div class="container text-center mt-3 mb-3">
+    <a href="#nl" class="btn btn-primary">Netherlands</a>
+    <a href="#gb" class="btn btn-primary">United Kingdom</a>
+    <a href="#es" class="btn btn-primary">Spain</a>
+    <a href="#de" class="btn btn-primary">Germany</a>
+</div>
+<div class="container mb-3" id="nl">
     <div class="row">
         <div class="col-md d-flex flex-column p-4">
+            <h2>Netherlands</h2>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    List item 1
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 2
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 3
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 4
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 5
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 6
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 7
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 8
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 9
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>   
+                <li class="list-group-item">
+                    List item 10
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>                                                                                                                                                
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="container mb-3" id="gb">
+    <div class="row">
+        <div class="col-md d-flex flex-column p-4">
+            <h2>United Kingdom</h2>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    List item 1
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 2
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 3
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 4
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 5
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 6
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 7
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 8
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 9
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>   
+                <li class="list-group-item">
+                    List item 10
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>                                                                                                                                                
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="container mb-3" id="es">
+    <div class="row">
+        <div class="col-md d-flex flex-column p-4">
+            <h2>Spain</h2>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    List item 1
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 2
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 3
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 4
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 5
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 6
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 7
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 8
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>
+                <li class="list-group-item">
+                    List item 9
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>   
+                <li class="list-group-item">
+                    List item 10
+                    <button class="btn btn-primary btn-sm float-end">link</button>
+                </li>                                                                                                                                                
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="container mb-3" id="de">
+    <div class="row">
+        <div class="col-md d-flex flex-column p-4">
+            <h2>Germany</h2>
             <ul class="list-group">
                 <li class="list-group-item">
                     List item 1

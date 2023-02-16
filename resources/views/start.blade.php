3e0abd919740080ad3f86ad84f28ea07d443c57f
Emma Vriezen <emma.vriezen@vlinderstichting.nl>
2023-02-16 11:37:03 +0100

    Explain difference of app and website on landing page

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -12,13 +12,13 @@ Welcome to InsectsCount
 <div class="container-fluid background-container d-flex">
     <div class="central-container" align="middle">
         <h1>Welcome to InsectsCount</h1>
-        <h2 align="middle">Select your device</h2>
-        <div align='middle'>
+        <h4 align="middle">Continue to the <a href='/index.html'>mobile app</a> to <b>count</b>, or <a href='/showLoginScreen'>website</a> to view your <b>data</b></h4>
+<!--         <div align='middle'>
             <a href='/index.html'>
                 <img src='/img/smartphone-icon.png' alt='smartphone icon' height='100' hspace='30' vspace='20'></a>
             <a href='/showLoginScreen'>
                 <img src='/img/computer-monitor-icon.png' alt='monitor icon' height='100' hspace='30' vspace='20'></a>
-        </div>
+        </div> -->
     </div>
 </div>
 @endsection
